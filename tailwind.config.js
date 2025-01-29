aa399f676a1ac6f5c52a4ab7604fc16cddab16bc
andrew.dev <andrew.devandcreatives@gmail.com>
2025-01-29 18:42:04 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 module.exports = {
-    theme: {
-      extend: {
-        fontFamily: {
-          geist: ['Geist', 'sans-serif'],
-          jersey: ['"Jersey 10"', 'serif'], 
-        },
+  theme: {
+    extend: {
+      screens: {
+        xs: '430px', 
+      },
+      fontFamily: {
+        geist: ['Geist', 'sans-serif'],
+        jersey: ['"Jersey 10"', 'serif'], 
       },
     },
-  };
-  
+  },
+};
